56f5fc0e6d7a8f0e562c1b580960fde9d79813bb
Dr-Ivan02 <20251875@itla.edu.do>
2026-01-23 17:53:13 -0400

    Nothing to say

---
+++ PromedioDeCalificaciones/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿//Programa que evalúe la condición del estudiante según su promedio, pero que también verifique los datos ingresados.
+
+int average = 0;
+bool averageVerification;
+do
+{
+    // Solicitud de promedio
+    Console.Write("Ingresar promedio (solo acepta números de 0 a 100): ");
+
+    //Verificación del dato ingresado, una vez validado, se guarda en average.
+    averageVerification = int.TryParse(Console.ReadLine(), out average);
+
+    //Dato ingresado incorrecto: Limpiar pantalla.
+    if (averageVerification == false || average > 100 || average < 0)
+    {
+        Console.Clear();
+    }
+
+} while (averageVerification == false || average > 100 || average < 0);
+
+//Condición del estudiante según el promedio.
+if (average > 90 && average < 101)
+{
+    Console.WriteLine("Excelente");
+}
+else if (average >= 70 && average < 90)
+{
+Console.WriteLine("Aprobado");
+}
+else
+{
+    Console.WriteLine("Reprobado");
+}
